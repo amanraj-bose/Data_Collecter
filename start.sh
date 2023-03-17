4193896863e8619d81646b35c34d5e583d168016
amanraj-bose <88227750+amanraj-bose@users.noreply.github.com>
2023-03-17 13:19:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-python3 main.py
+
+set -eu
+export PYTHONUNBUFFERED=true
+VIRTUALENV=.data/venv
+if [ ! -d $VIRTUALENV ]; then
+    python3 -m venv $VIRTUALENV
+fi
+
+if [ ! -f $VIRTUALENV/bin/pip ]; then
+    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
+fi
+
+$VIRTUALENV/bin/pip install -r requirements.txt
+
+$VIRTUALENV/bin/python3 maim.py
