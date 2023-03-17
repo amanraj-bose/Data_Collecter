9bebd675bb7a0ce1caac7045c82ef467b0a81490
amanraj-bose <88227750+amanraj-bose@users.noreply.github.com>
2023-03-17 22:15:13 +0530

    Create wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,8 @@
+from main import app as application
+from main import db
+
+if __name__ == '__main__':
+    application.app_context().push()
+    db.drop_all()
+    db.create_all()
+    application.run()
