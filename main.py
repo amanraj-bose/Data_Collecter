4c7f04d11fcbcdc18909b2b94fe5d28b1b36906e
amanraj-bose <88227750+amanraj-bose@users.noreply.github.com>
2023-03-18 11:00:19 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -4,8 +4,8 @@
     @File Created => 02/02/2023
 """
 
-from modules import SentenceCheck, PreProcessor, Log
-from modules import sprint, Filter, log_clear
+from modules import SentenceCheck, PreProcessor #, Log
+from modules import sprint, Filter #, log_clear
 from modules import DataBaseCreatorCleaner
 from modules import path
 from modules import encode, decode
@@ -41,7 +41,7 @@ app.config["SECRET_KEY"] = 'amanr@j_bose9077'
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-    os.path.join(basedir, "database", "database.db")
+    os.path.join(basedir,  "database.db")
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -49,13 +49,13 @@ db = SQLAlchemy(app)
 
 MAX_VALUE_RATE = 5
 json_files = Links(directory='meta', file=['links.json', 'today.agenda.json'])
-log = []
+# log = []
 
 ######################################################################
 
 # Log Cleaner
 
-log_path = os.path.join(path(os.path.abspath(os.path.dirname(__file__))), 'Data_Collecter', 'log')
+# log_path = os.path.join(path(os.path.abspath(os.path.dirname(__file__))), 'Data_Collecter', 'log')
 # log_clear(path=log_path) # , 'system.log'
 
 ###########################################################
@@ -78,8 +78,8 @@ class DataBase(db.Model):
 
 @app.route('/')
 def home():
-    log.append(
-        f"[Info] [{request.remote_addr}] [{time.asctime()}] User Visited on your Website\n")
+    # log.append(f"[Info] [{request.remote_addr}] [{time.asctime()}] User Visited on your Website\n")
+
     return render_template(r'index.html',
                            api=json_files.API,
                            github=json_files.GITHUB,
@@ -96,8 +96,9 @@ def home():
 
 @app.route('/collector', methods=['GET', 'POST'])
 def collector():
-    logger = Log(os.path.join(log_path, "system.log"))
+    #logger = Log(os.path.join(log_path, "system.log"))
     submited = ""
+
     if request.method == "POST":
         filters = Filter()
         verifier = SentenceCheck()
@@ -113,35 +114,36 @@ def collector():
                     data.text_data = for_database
                     db.session.add(data)
                     db.session.commit()
-                    log.append(
-                        f"[Info] [{request.remote_addr}] [{time.asctime()}] DataBase OverWrited\n")
-                    logger.log(log)
+                    #log.append(f"[Info] [{request.remote_addr}] [{time.asctime()}] DataBase OverWrited\n")
+                    # logger.log(log)
                 else:
                     flash("Length Error")
-                    log.append(
-                        f"[Error] [{request.remote_addr}] [{time.asctime()}] Length Of the sentence is Too short\n")
-                    logger.log(log)
+                    # log.append( f"[Error] [{request.remote_addr}] [{time.asctime()}] Length Of the sentence is Too short\n")
+                    # logger.log(log)
             else:
                 flash("Sentiment Error")
-                log.append(
-                    f"[Error] [{request.remote_addr}] [{time.asctime()}] Error Text Type\n")
-                logger.log(log)
-            return redirect(url_for('collector'))
+                #log.append(
+                #    f"[Error] [{request.remote_addr}] [{time.asctime()}] Error Text Type\n")
+                #logger.log(log)
+
+            # return redirect(url_for('collector'))
+
         else:
             flash("It is a Result Panel More info Click `Form` navlink.")
-            log.append(
-                f"[Error] [{request.remote_addr}] [{time.asctime()}] First Layer Stop the Input\n")
-            logger.log(log)
+            #log.append(
+                #f"[Error] [{request.remote_addr}] [{time.asctime()}] First Layer Stop the Input\n")
+            #logger.log(log)
 
-        log.append(
-            f"[Info] [{request.remote_addr}] [{time.asctime()}] User Visited on your Website '/collector' \n")
-        logger.close()
+        #log.append(
+            #f"[Info] [{request.remote_addr}] [{time.asctime()}] User Visited on your Website '/collector' \n")
+        #logger.close()
 
-        return redirect(url_for('collector'))
+        return render_template(r'collector.html', twitter=json_files.TWITTER, github=json_files.GITHUB, home="/", collector="/collector", agenda=str(json_files.AGENDA_CONTENT + ".").capitalize(), Identity=submited, data="/07397d633f25a7101990a75864ae03d5a3b9ac07c4ed6accbc52cbfd7d7c13b4", time_cool_down=json_files.COOL_DOWN_TIME, about="/a4262e1c9bcbc1721eb3fe13558154460a2b2a2d307daa32532478526ce6ccb1")
+
 
     elif request.method == "GET":
-        log.append(
-            f"[Info] [{request.remote_addr}] [{time.asctime()}] User Visited on your Website '\colledctor' \n")
+        # log.append(
+          #  f"[Info] [{request.remote_addr}] [{time.asctime()}] User Visited on your Website '\colledctor' \n")
         return render_template(r'collector.html', twitter=json_files.TWITTER, github=json_files.GITHUB, home="/", collector="/collector", agenda=str(json_files.AGENDA_CONTENT + ".").capitalize(), Identity=submited, data="/07397d633f25a7101990a75864ae03d5a3b9ac07c4ed6accbc52cbfd7d7c13b4", time_cool_down=json_files.COOL_DOWN_TIME, about="/a4262e1c9bcbc1721eb3fe13558154460a2b2a2d307daa32532478526ce6ccb1")
 
 
@@ -153,14 +155,14 @@ def data():
 
 @app.route("/data_download", methods=['GET', 'POST'])
 def data_download():
-    log.append(f"[Info] [{request.remote_addr}] [{time.asctime()}] Data Download By any User")
+    #log.append(f"[Info] [{request.remote_addr}] [{time.asctime()}] Data Download By any User")
     if request.method == "POST":
         try:
-            os.mkdir(os.path.join(path(os.path.abspath(os.path.dirname(__file__))), "app", "data"))
+            os.mkdir(os.path.join(path(os.path.abspath(os.path.dirname(__file__))), "Data_Collecter", "data"))
         except Exception:
             pass
 
-        to_csv_file = os.path.join(path(os.path.abspath(os.path.dirname(__file__))), "app", "data", "database.csv")
+        to_csv_file = os.path.join(path(os.path.abspath(os.path.dirname(__file__))), "Data_Collecter", "data", "database.csv")
 
         with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "meta", "login.secret.json"), "r") as f:
             login = json.load(f)['login']
@@ -168,10 +170,10 @@ def data_download():
         username = request.form.get("userPass", type=str)
         password = request.form.get("passval", type=str)
         if (username == login['username'] and password == login['password']):
-            log.append(f"[Warning] UserName >> {encode(username)}, Password >> {encode(password)}".format(
-            username=username, password=password))
+            #log.append(f"[Warning] UserName >> {encode(username)}, Password >> {encode(password)}".format(
+            #username=username, password=password))
 
-            conn = connect(os.path.join(basedir, "database", "database.db"))
+            conn = connect(os.path.join(basedir, "database.db"))
             service = pd.read_sql("SELECT * FROM data_base", conn)
             service.to_csv(to_csv_file, index=False)
             return send_file(
@@ -194,8 +196,8 @@ def about():
     return render_template(r'about.html', about="/a4262e1c9bcbc1721eb3fe13558154460a2b2a2d307daa32532478526ce6ccb1", data="/07397d633f25a7101990a75864ae03d5a3b9ac07c4ed6accbc52cbfd7d7c13b4", home="/", collector="/collector", twitter=json_files.TWITTER, github=json_files.GITHUB, author="Aman Raj",
     github_author_1="", twitter_author_1="")
 
-# if __name__ == '__main__':
-#     app.app_context().push()
-#     db.drop_all()
-#     db.create_all()
-#     app.run(host="0.0.0.0", port=4000)
+if __name__ == '__main__':
+    # app.app_context().push()
+    # db.drop_all()
+    # db.create_all()
+    app.run(host="0.0.0.0", port=4000)
